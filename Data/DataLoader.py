d9da37eb21d65f55f1f1c6838a5dcced00657e90
Mason Wheeler <wheels@MasonsMacStudio.domain>
2023-07-24 18:44:07 -0700

    fixing utilities bug

--- Data/DataLoader.py
+++ Data/DataLoader.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
+
+from Data.Data import get_and_process_data
+
+
 class DataLoader:
-    def __init__(self, X, Y, x_size, y_size, window_lookback_size, window_lookforward_size):
+    def __init__(self, X, x_size = 128, y_size = 128, window_lookback_size=400, window_lookforward_size=400):
         self.X = X
-        self.Y = Y
         self.x_size = x_size
         self.y_size = y_size
         self.window_lookback_size = window_lookback_size
@@ -23,23 +26,35 @@ class DataLoader:
 
         # Get Y indices
         y_indices = np.array([index + int(step) for step in y_steps])
-        y_indices = np.clip(y_indices, 0, len(self.Y) - 1)  # ensure indices are within bounds
+        y_indices = np.clip(y_indices, 0, len(self.X) - 1)  # ensure indices are within bounds
 
         # Fetch X and Y data
         x_data = self.X[x_indices]
-        y_data = self.Y[y_indices]
+        y_data = self.X[y_indices]
 
         return x_data, y_data
 
+    def generate_training_data(self):
+
+        X_data = []
+        Y_data = []
+
+        for i in range(self.window_lookback_size, len(self.X) - self.window_lookforward_size):
+            x_data, y_data = self.get_data(i)
+            X_data.append(x_data)
+            Y_data.append(y_data)
+
+        return np.array(X_data), np.array(Y_data)
+
+
 if __name__ == "__main__":
-    # Create some dummy data
-    X = np.arange(1000)
-    Y = np.arange(1000)
+    data, scaled_data, scaler = get_and_process_data('AAPL', '1Min', 128, month='2023-07')
 
     # Create a data loader
-    data_loader = DataLoader(X, Y, 128, 30, 400, 400)
+    data_loader = DataLoader(scaled_data)
 
-    # Get some data
-    x_data, y_data = data_loader.get_data(500)
-    print(x_data)
-    print(y_data)
+    # Generate training data
+    X_train, Y_train = data_loader.generate_training_data()
+
+    print(X_train.shape)
+    print(Y_train.shape)
