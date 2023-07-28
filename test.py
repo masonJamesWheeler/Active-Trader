5ae4bf6a9a54b10398ffcc0d23931288787aca68
Mason Wheeler <wheels@MasonsMacStudio.domain>
2023-07-28 11:00:55 -0700

    saving the TiDE model before I butcher it

--- test.py
+++ test.py
@@ -29,110 +29,133 @@ ts = TimeSeries(key=alpha_vantage_api_key, output_format='pandas')
 ti = TechIndicators(key=alpha_vantage_api_key, output_format='pandas')
 
 
-
 class ResidualBlock(nn.Module):
     def __init__(self, in_features, out_features):
         super(ResidualBlock, self).__init__()
-        self.linear1 = nn.Linear(in_features, out_features)
-        self.linear2 = nn.Linear(out_features, out_features)
-        self.shortcut = nn.Linear(in_features, out_features)  # new shortcut connection
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
-        self.layer_norm = nn.LayerNorm(out_features)
+        self.linear1 = nn.Linear(in_features, out_features) # Initial linear layer to map input to specified out_features.
+        self.linear2 = nn.Linear(out_features, out_features) # Another linear layer to further process the input.
+        self.shortcut = nn.Linear(in_features, out_features) # Shortcut connection to allow gradients to flow back easily.
+        self.relu = nn.ReLU() # Activation function to introduce non-linearity.
+        self.dropout = nn.Dropout(0.5) # Dropout for regularization to prevent overfitting.
+        self.layer_norm = nn.LayerNorm(out_features) # Layer normalization to stabilize the learning process.
 
     def forward(self, x):
-        residual = self.shortcut(x)  # apply the shortcut connection to the input
-        out = self.relu(self.linear1(x))
-        out = self.dropout(self.linear2(out))
-        out += residual
-        out = self.layer_norm(out)
-        return out
+        residual = self.shortcut(x) # Apply the shortcut connection.
+        out = self.relu(self.linear1(x)) # Apply ReLU after first linear layer.
+        out = self.dropout(self.linear2(out)) # Apply dropout after second linear layer.
+        out += residual # Add the original input (residual) to the output of the layers.
+        out = self.layer_norm(out) # Apply layer normalization.
+        return out # Return the output.
 
 class Encoder(nn.Module):
     def __init__(self, input_size, output_dim, num_layers):
         super(Encoder, self).__init__()
-        self.layers = nn.ModuleList([ResidualBlock(input_size if i==0 else output_dim, output_dim) for i in range(num_layers)])
+        self.layers = nn.ModuleList([ResidualBlock(input_size if i==0 else output_dim, output_dim) for i in range(num_layers)]) # Encoder is composed of a sequence of ResidualBlocks.
 
     def forward(self, x):
-        for layer in self.layers:
+        for layer in self.layers: # Pass the input through each layer.
             x = layer(x)
-        return x
+        return x # Return the output after passing through all layers.
 
 class Decoder(nn.Module):
     def __init__(self, input_size, num_layers, output_dim):
         super(Decoder, self).__init__()
-        self.layers = nn.ModuleList([ResidualBlock(input_size if i==0 else output_dim, output_dim) for i in range(num_layers)])
-        self.linear = nn.Linear(output_dim, output_dim)
+        self.layers = nn.ModuleList([ResidualBlock(input_size if i==0 else output_dim, output_dim) for i in range(num_layers)]) # Decoder is also composed of a sequence of ResidualBlocks.
+        self.linear = nn.Linear(output_dim, output_dim) # Final linear layer to map the output to the desired output dimension.
 
     def forward(self, x):
-        for layer in self.layers:
+        for layer in self.layers: # Pass the input through each layer.
             x = layer(x)
-        return self.linear(x)
+        return self.linear(x) # Apply the final linear layer and return the output.
 
 class FeatureProjection(nn.Module):
     def __init__(self, in_features, out_features):
         super(FeatureProjection, self).__init__()
-        self.linear = nn.Linear(in_features, out_features)
+        self.linear = nn.Linear(in_features, out_features) # Linear layer to project the input features to a different dimension.
 
     def forward(self, x):
-        return self.linear(x)
+        return self.linear(x) # Apply the linear layer and return the output.
 
 class TemporalAttention(nn.Module):
     def __init__(self, hidden_dim):
         super(TemporalAttention, self).__init__()
-        self.W = nn.Linear(hidden_dim, hidden_dim, bias=False)
-        self.v = nn.Linear(hidden_dim, 1, bias=False)
+        self.W = nn.Linear(hidden_dim, hidden_dim, bias=False) # Linear layer to transform the input before applying attention.
+        self.v = nn.Linear(hidden_dim, 1, bias=False) # Linear layer to calculate attention scores.
 
     def forward(self, encoder_outputs):
         # encoder_outputs shape: [batch_size, sequence_len, hidden_dim]
-        energy = self.v(torch.tanh(self.W(encoder_outputs)))  # shape: [batch_size, sequence_len, 1]
-        attention = F.softmax(energy, dim=1)  # shape: [batch_size, sequence_len, 1]
+        energy = self.v(torch.tanh(self.W(encoder_outputs)))  # Apply transformation and tanh activation, then calculate attention scores.
+        attention = F.softmax(energy, dim=1)  # Apply softmax to get attention distribution.
         return attention
 
 class TemporalDecoder(nn.Module):
     def __init__(self, in_features, out_features, covariate_dim, hidden_dim, output_len):
         super(TemporalDecoder, self).__init__()
-        self.attention = TemporalAttention(hidden_dim)
-        self.residual_block = ResidualBlock(2 * hidden_dim, out_features)  # changed hidden_dim to output_dim
-        self.covariate_projection = nn.Linear(covariate_dim, hidden_dim)  # changed out_features to hidden_dim
+        self.attention = TemporalAttention(hidden_dim) # Temporal attention mechanism.
+        self.residual_block = ResidualBlock(2 * hidden_dim, out_features)  # Residual block for processing inputs.
+        self.covariate_projection = nn.Linear(covariate_dim, hidden_dim)  # Linear layer for projecting covariates.
         self.output_len = output_len
 
     def forward(self, x, projected_covariates):
         outputs = torch.zeros(x.size(0), self.output_len, 5).to(x.device)  # initialize output tensor
-        for t in range(self.output_len):
-            attention = self.attention(x)
-            context = torch.sum(attention * x, dim=1)  # shape: [batch_size, hidden_dim]
-            context = context.unsqueeze(1)  # shape: [batch_size, 1, hidden_dim]
-            projected_covariate = self.covariate_projection(projected_covariates[:, t, :]).unsqueeze(
-                1)  # apply projection
-            x = self.residual_block(
-                torch.cat((context, projected_covariate), dim=-1))  # shape: [batch_size, 1, hidden_dim]
-            outputs[:, t, :] = x.squeeze(1)
+        for t in range(self.output_len): # For each time step in the output sequence.
+            attention = self.attention(x) # Calculate the attention weights.
+            context = torch.sum(attention * x, dim=1)  # Calculate the context vector.
+            context = context.unsqueeze(1)  # Add an extra dimension to match the tensor sizes.
+            projected_covariate = self.covariate_projection(projected_covariates[:, t, :]).unsqueeze(1)  # Project the covariates and add an extra dimension.
+            x = self.residual_block(torch.cat((context, projected_covariate), dim=-1))  # Concatenate context and projected_covariates, then apply residual_block.
+            outputs[:, t, :] = x.squeeze(1) # Remove the extra dimension and add the output to the outputs tensor.
         return outputs
 
 
 class TiDE(nn.Module):
     def __init__(self, input_size, num_encoder_layers, num_decoder_layers, output_dim, projected_dim):
         super(TiDE, self).__init__()
+        # The FeatureProjection layer is used to project the input features into a different dimension. This is typically used when we want to reduce the dimensionality of our input.
         self.feature_projection = FeatureProjection(input_size, projected_dim)
+
+        # The Encoder consists of a number of ResidualBlocks that are designed to encode the input sequence into a lower-dimensional representation.
         self.encoder = Encoder(projected_dim, output_dim, num_encoder_layers)
+
+        # The Decoder also consists of a number of ResidualBlocks that are designed to decode the low-dimensional representation back into the original dimension. In this case, it is used to decode the encoded sequence into a prediction for the next time step.
         self.dense_decoder = Decoder(output_dim, num_decoder_layers, output_dim)
+
+        # The TemporalDecoder is designed to process the sequence in a time-dependent manner. It applies attention to the sequence at each time step and uses the context vector and the projected covariates to predict the output at that time step.
         self.temporal_decoder = TemporalDecoder(output_dim, output_dim, input_size, output_dim, 10)
+
+        # The global_residual_connection is a linear layer used to create a shortcut connection from the input to the output. This is used in the global attention mechanism to help retain information from the input that might be lost during the encoding and decoding processes.
         self.global_residual_connection = nn.Linear(input_size, output_dim)
+
+        # The global_attention is an attention mechanism that is applied to the output of the global_residual_connection. This helps the model to focus on the most important parts of the input when making its predictions.
         self.global_attention = TemporalAttention(output_dim)  # added this line
 
     def forward(self, x, covariates):
+        # Project the input features into a different dimension using the feature_projection layer.
         projected_x = self.feature_projection(x)
+
+        # Encode the projected input into a lower-dimensional representation using the encoder.
         encoded_x = self.encoder(projected_x)
+
+        # Decode the encoded representation back into the original dimension using the decoder. This provides a dense prediction for the next time step.
         decoded_x = self.dense_decoder(encoded_x)
+
+        # Apply the temporal decoder to the encoded sequence and the covariates. This provides a time-dependent prediction for the next time step.
         final_output = self.temporal_decoder(encoded_x, covariates)
 
-        # apply global attention to reduce sequence length
+        # Apply global attention to reduce sequence length
+        # First, apply the global_residual_connection to the input to create a shortcut connection.
         global_residual = self.global_residual_connection(x)
+
+        # Then, apply the global_attention to the output of the global_residual_connection. This produces an attention distribution over the input sequence.
         attention = self.global_attention(global_residual)
+
+        # Multiply the attention distribution with the global_residual to get a weighted sum of the input features. This gives more importance to the features that the attention mechanism thinks are more relevant.
         global_residual = torch.sum(attention * global_residual, dim=1)  # shape: [batch_size, hidden_dim]
+
+        # Expand the global_residual to match the shape of the final_output. This is done so that the global_residual can be added to the final_output.
         global_residual = global_residual.unsqueeze(1).expand(-1, 10, -1)  # shape: [batch_size, 10, hidden_dim]
 
+        # Finally, add the global_residual to the final_output. This allows the model to retain some of the information from the input that might have been lost during the encoding and decoding processes.
         return final_output + global_residual
 
     def save_weights(self, ticker, epoch, live=True):
